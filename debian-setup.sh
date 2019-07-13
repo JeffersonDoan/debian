54a27b5d3e43ad3857832956895c71a08cd25266
Jefferson Doan <52799595+JeffersonDoan@users.noreply.github.com>
2019-07-13 01:45:41 -0400

    Update debian-setup.sh

--- debian-setup.sh
+++ debian-setup.sh
@@ -5,12 +5,12 @@ cd ~
 # Retrieve data for the setup script
 wget https://raw.githubusercontent.com/JeffersonDoan/debian-setup/master/debian.packages
 wget https://raw.githubusercontent.com/JeffersonDoan/debian-setup/master/sources.list
-wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2012.05.05_all.deb -O deb-multimedia-keyring.deb
+wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
 
 # Add Firefox, Spotify, and Multimedia key
 sudo apt-key adv --keysever ha.pool.sks-keyservers.net --recv-keys 85A3D26506C4AE2A
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90 
-sudo dpkg -i multimedia-keyring_all.deb
+sudo dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
 
 # Create backup and install repositories
 sudo cp /etc/apt/sources.list{,.bak}
@@ -21,7 +21,7 @@ rm ./sources.list
 sudo apt update
 
 # Full pack upgrade
-sudo apt upgrade -y
+sudo apt dist-upgrade -y
 
 # Install Joplin
 wget https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh && sudo chmod +x Joplin_install_and_update.sh && ./Joplin_install_and_update.sh
