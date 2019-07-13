343959dcb60b9df6473f2b2dba588335540e499a
Jefferson Doan <52799595+JeffersonDoan@users.noreply.github.com>
2019-07-13 00:36:53 -0400

    Update debian-setup.sh

--- debian-setup.sh
+++ debian-setup.sh
@@ -3,34 +3,42 @@
 cd ~
 
 # Retrieve data for the setup script
-#wget https://raw.githubusercontent.com/JeffersonDoan/fedora-setup/master/debian.packages
-
-# Install repositories
-sudo cp /etc/apt/sources.list{,.bak}
+wget https://raw.githubusercontent.com/JeffersonDoan/debian-setup/master/debian.packages
 wget https://raw.githubusercontent.com/JeffersonDoan/debian-setup/master/sources.list
+
+# Add Firefox key
+sudo apt-key adv --keysever ha.pool.sks-keyservers.net --recv-keys 85A3D26506C4AE2A
+
+# Create backup and install repositories
+sudo cp /etc/apt/sources.list{,.bak}
 sudo cp ./sources.list /etc/apt/sources.list
+rm ./sources.list
 
 
-# Update cache for package installs
-#sudo dnf makecache
+# Update to install multimedia keyring
+sudo apt update -oAcquire::AllowInsecureRepositories=true
 
-# Install TeamViewer
-#sudo dnf install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -y
+# Install multimedia keyring
+sudo apt install deb-multimedia-keyring -y
+
+# Full package update
+sudo apt update
+
+# Full pack upgrade
+sudo apt upgrade -y
 
 # Install Joplin
-#wget https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh && sudo chmod +x Joplin_install_and_update.sh && ./Joplin_install_and_update.sh
+wget https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh && sudo chmod +x Joplin_install_and_update.sh && ./Joplin_install_and_update.sh
 
-# Install packages from fedora.packages
-#sudo dnf install $(cat ~/fedora.packages) -y
-
-# Install Flatpaks from fedora.flatpak
-#flatpak install flathub $(cat ~/fedora.flatpaks) -y
-
-# Custom shell prompt with aliases - source: https://www.linuxquestions.org/questions/linux-general-1/ultimate-prompt-and-bashrc-file-4175518169/
-#cat ~/bashrc.aliases >> ~/.bashrc
+# Install packages from debian.packages
+sudo apt install $(cat ~/debian.packages) -y
 
 # Disable Wayland and use Xorg
-#sudo sed -i '/WaylandEnable/s/^#//g' /etc/gdm/custom.conf
+sudo sed -i '/WaylandEnable/s/^#//g' /etc/gdm3/daemon.conf
 
-# Update the rest of the system
-#sudo dnf update -y
+# Remove Firefox ESR and GNOME Games
+sudo apt remove firefox-esr -y
+sudo apt autoremove gnome-games -y
+
+# Cleanup package dependencies
+sudo apt autoremove
